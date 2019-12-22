ea8f4059edc823c1ae303621a45e338739e88de5
jmfl1129 <lee1998_90@yahoo.com>
2019-12-23 05:20:23 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -4,6 +4,7 @@ setcookie('logged', '', time() - 3600);
 setcookie('email', '', time() - 3600);
 setcookie('id', '', time() - 3600);
 setcookie('name', '', time() - 3600);
+setcookie('organizer', '', time() - 3600);
 
 header("Location: /index.php");
 ?>
