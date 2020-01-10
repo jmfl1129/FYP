bc8144e3357a370fea7cda996f19015b5037a45b
jmfl1129 <lee1998_90@yahoo.com>
2020-01-10 20:07:25 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -16,7 +16,7 @@ $statusMsg = '';
 $targetDir = "uploads/";
 $fileName = basename($_FILES["file"]["name"]);
 $targetFilePath = $targetDir . $fileName;
-$fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION);
+$fileType = strtolower(pathinfo($targetFilePath,PATHINFO_EXTENSION));
 
 if(isset($_POST["submit"]) && !empty($_FILES["file"]["name"])){
     // Allow certain file formats
